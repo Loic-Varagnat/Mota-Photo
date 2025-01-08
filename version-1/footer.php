5f0152f55a84818c1029d808867cb053829798e9
Loic-Varagnat <loicvaragnat74@hotmail.com>
2025-01-08 12:34:51 +0100

    Delete version-1 directory

--- version-1/footer.php
+++
@@ -1,8 +0,0 @@
-<footer>
-    <section class="footer_container">
-
-        <div class="footer_mention"><a  href="">mentions légales</a></div>
-        <div class="footer_vp"><a   href="">vie privée</a></div>
-        <div class="footer_droit"><a   href="">tous droits réservés</a></div>
-    </section>
-</footer>
